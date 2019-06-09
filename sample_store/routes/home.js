1475e8a15b1307d938af04725bf95e4f778bdb2b
Nicolás Proto <nicolasproto@gmail.com>
2019-06-09 17:24:13 -0300

    App configuration

--- sample_store/routes/home.js
+++ sample_store/routes/home.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-	res.send('This is the home router!')
+
+	res.render('home', null)
 
 })
 
--- sample_store/server.js
+++ sample_store/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const path = require('path')
 const home = require('./routes/home')
 
 const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hjs')
 
 app.use('/', home)
 
