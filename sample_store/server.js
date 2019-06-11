51bf4af6a7263fe4bf84b71d4b3f35090bbf207d
Nicolás Proto <nicolasproto@gmail.com>
2019-06-10 23:03:08 -0300

    Registration form

--- sample_store/server.js
+++ sample_store/server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const path = require('path')
 const home = require('./routes/home')
+const register = require('./routes/register')
+
 
 const app = express()
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hjs')
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', home)
+app.use('/register', register)
 
 
 app.listen(5000)
